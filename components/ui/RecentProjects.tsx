c3d8c1fca4510aad82b8655418abad7c29dcc8d5
Ho Trung <khanhtrung14111999@gmail.com>
2024-06-05 15:48:13 +0300

    landing page with box content and customize CSS

---
+++ components/ui/RecentProjects.tsx
@@ -0,0 +1,23 @@
+import { projects } from '@/data'
+import React from 'react'
+
+const RecentProjects = () => {
+    return (
+        <div className='py-20'>
+            <h1 className='heading'>
+                A small selection of {' '}
+                <span className='text-purple'>recent projects</span>
+
+            </h1>
+            <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
+                {projects.map((project) =>(
+                    <div>
+                        {project.title}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default RecentProjects
