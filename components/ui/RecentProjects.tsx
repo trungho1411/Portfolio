f09887dff55ba33cf946519d1f1ed69a04aa164e
Ho Trung <khanhtrung14111999@gmail.com>
2024-06-05 17:53:27 +0300

    put the recent projects template and 3d-pin animation

--- components/ui/RecentProjects.tsx
+++ components/ui/RecentProjects.tsx
@@ -1,5 +1,6 @@
 import { projects } from '@/data'
 import React from 'react'
+import { PinContainer } from './3d-pin'
 
 const RecentProjects = () => {
     return (
@@ -10,10 +11,35 @@ const RecentProjects = () => {
 
             </h1>
             <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
-                {projects.map((project) =>(
-                    <div>
-                        {project.title}
+                {projects.map(({ id, title, des, img, iconLists, link }) => (
+                    <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
+                        <PinContainer title={title} href={link}>
+                            <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10'>
+                                <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
+                                    <img src='/bg.png' alt='bg-img' />
+                                </div>
+                                <img
+                                    src={img}
+                                    alt={title}
+                                    className='z-10 absolute bottom-0'
+                                />
+                            </div>
+                            <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
+                                {title}
+                            </h1>
+
+                            <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
+                                {des}
+                            </p>
+                            <div className='flex items-center justify-between mt-7 mb-3'>
+                                <div className='flex items-center'>
+
+                                </div>
+
+                            </div>
+                        </PinContainer>
                     </div>
+
                 ))}
             </div>
         </div>
