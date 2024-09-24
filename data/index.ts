0280904645d3d4743711608ff990efd6ef1492a9
Ho Trung <khanhtrung14111999@gmail.com>
2024-09-24 19:16:58 +0300

    command undone projects

--- data/index.ts
+++ data/index.ts
@@ -86,22 +86,22 @@ export const projects = [
     iconLists: ['/re.svg', '/next.svg', '/ts.svg', '/shadcn.svg'],
     link: 'https://github.com/trungho1411/Healthcare-System-Application',
   },
-  {
-    id: 3,
-    title: 'AI Image SaaS - Canva Application',
-    des: 'A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.',
-    img: '/p3.svg',
-    iconLists: ['/re.svg', '/tail.svg', '/ts.svg', '/three.svg', '/c.svg'],
-    link: 'https://github.com/adrianhajdin/ai_saas_app',
-  },
-  {
-    id: 4,
-    title: 'Animated Apple Iphone 3D Website',
-    des: 'Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..',
-    img: '/p4.svg',
-    iconLists: ['/next.svg', '/tail.svg', '/ts.svg', '/three.svg', '/gsap.svg'],
-    link: 'https://github.com/adrianhajdin/iphone',
-  },
+  // {
+  //   id: 3,
+  //   title: 'AI Image SaaS - Canva Application',
+  //   des: 'A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.',
+  //   img: '/p3.svg',
+  //   iconLists: ['/re.svg', '/tail.svg', '/ts.svg', '/three.svg', '/c.svg'],
+  //   link: 'https://github.com/adrianhajdin/ai_saas_app',
+  // },
+  // {
+  //   id: 4,
+  //   title: 'Animated Apple Iphone 3D Website',
+  //   des: 'Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..',
+  //   img: '/p4.svg',
+  //   iconLists: ['/next.svg', '/tail.svg', '/ts.svg', '/three.svg', '/gsap.svg'],
+  //   link: 'https://github.com/adrianhajdin/iphone',
+  // },
 ];
 
 export const companies = [
